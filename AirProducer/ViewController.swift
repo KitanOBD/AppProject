34dcb871ece104f3e25b9c92caab374c547177ba
KitanOBD <kitan.o.122@hotmail.com>
2017-05-28 20:24:12 -0400

    1. Start App
    Started App Dev.
    
    Installed Firebase & Pods.
    
    Created:
    
    - intro video
    - text overlay
    
    Building Successfully.
    
    Next steps: To add user sign up.

--- AirProducer/ViewController.swift
+++ AirProducer/ViewController.swift
@@ -7,11 +7,24 @@
 //
 
 import UIKit
+import AVKit
+import Firebase
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet var videoView: UIView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad(
+        
+        
+        self.setupView()
+        
+        
+        
+        )
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +33,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    func setupView() {
+    
+        let path = NSURL(fileURLWithPath: Bundle.main.path(forResource: "IntroMov", ofType: "mp4")!)
+        
+        let player = AVPlayer(url: path as URL)
+        
+        let newLayer = AVPlayerLayer(player: player)
+        newLayer.frame = self.videoView.frame
+        self.videoView.layer.addSublayer(newLayer)
+        newLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        self.airProdTextCenter(containerView: self.videoView)
+        player.play()
+        player.actionAtItemEnd = AVPlayerActionAtItemEnd.none
+        
+        
+        NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: player.currentItem, queue: nil, using: { (_) in
+            DispatchQueue.main.async {
+                player.seek(to: kCMTimeZero)
+                player.play()
+            }
+        })
+    
+    }
+    
+    func airProdTextCenter(containerView: UIView){
+    
+    
+        let half:CGFloat = 1.0 / 2.0
+        
+        let airProdLabel = UILabel()
+        
+        airProdLabel.text = "AIR PROD"
+        airProdLabel.font = UIFont(name: "ArialRoundedMTBold", size: 40)
+        airProdLabel.backgroundColor = UIColor.clear
+        airProdLabel.textColor = UIColor.white
+        airProdLabel.sizeToFit()
+        airProdLabel.textAlignment = NSTextAlignment.center
+        airProdLabel.frame.origin.x = (containerView.frame.size.width - airProdLabel.frame.size.width) * half
+        airProdLabel.frame.origin.y = (containerView.frame.size.height - airProdLabel.frame.size.height) * half
+        containerView.addSubview(airProdLabel)
+        
+    }
 }
 
---
+++ Pods/Target Support Files/Pods-AirProducer/Pods-AirProducer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AirProducer : NSObject
+@end
+@implementation PodsDummy_Pods_AirProducer
+@end
